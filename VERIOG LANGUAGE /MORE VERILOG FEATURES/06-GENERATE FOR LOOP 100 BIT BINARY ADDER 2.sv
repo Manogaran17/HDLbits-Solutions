69fceb3a8c74ed64843fce06fc90465a67545dff
Manogaran17 <manogaran2667@gmail.com>
2026-04-12 00:36:26 +0530

    Update 06-GENERATE FOR LOOP 100 BIT BINARY ADDER 2.sv

--- VERIOG LANGUAGE /MORE VERILOG FEATURES/06-GENERATE FOR LOOP 100 BIT BINARY ADDER 2.sv
+++ VERIOG LANGUAGE /MORE VERILOG FEATURES/06-GENERATE FOR LOOP 100 BIT BINARY ADDER 2.sv
@@ -1,1 +1,27 @@
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+    
+    genvar i;
+    
+    fa FA(a[0],b[0],cin,sum[0],cout[0]);
+    generate 
+        for(i=1;i<100;i=i+1) begin:full_adder
+            fa FA(a[i],b[i],cout[i-1],sum[i],cout[i]);  
+        end  
+    endgenerate
+    
+endmodule
+
+module fa (a,b,cin,s,cout);
+    input a,b,cin;
+    output s,cout;
+    
+    assign s=(a^b^cin);
+    assign cout=(a&b)|cin&(a^b);
+endmodule 
+
+    
 
