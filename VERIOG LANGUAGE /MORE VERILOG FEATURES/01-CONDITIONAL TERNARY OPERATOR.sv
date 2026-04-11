818083a697c46252f35a05f183a4c94249f896cd
Manogaran17 <manogaran2667@gmail.com>
2026-04-11 23:49:26 +0530

    Update 01-CONDITIONAL TERNARY OPERATOR.sv

--- VERIOG LANGUAGE /MORE VERILOG FEATURES/01-CONDITIONAL TERNARY OPERATOR.sv
+++ VERIOG LANGUAGE /MORE VERILOG FEATURES/01-CONDITIONAL TERNARY OPERATOR.sv
@@ -1,1 +1,11 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);//
+    wire [7:0]num1,num2;
+    
+    assign num1=(a>b)?b:a;
+    assign num2=(num1>c)?c:num1;
+    assign min=(num2>d)?d:num2;
+    // assign intermediate_result1 = compare? true: false;
 
+endmodule
