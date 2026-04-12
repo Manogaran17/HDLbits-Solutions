389d760d3ce952baf99fa64678b46908ae13051c
Manogaran17 <manogaran2667@gmail.com>
2026-04-13 00:11:44 +0530

    Update 12-COMBINE CIRCUIT A AND B.sv

--- CIRCUITS/01-COMBINATIONAL LOGIC/01-BASIC GATES/12-COMBINE CIRCUIT A AND B.sv
+++ CIRCUITS/01-COMBINATIONAL LOGIC/01-BASIC GATES/12-COMBINE CIRCUIT A AND B.sv
@@ -1,1 +1,10 @@
+module top_module (input x, input y, output z);
+    wire w1,w2,w3,w4;
 
+    assign w1=(x^y)&x;
+    assign w2=~(x^y);
+    or(w3,w1,w2);
+    and(w4,w1,w2);
+    xor(z,w3,w4);
+    
+endmodule
