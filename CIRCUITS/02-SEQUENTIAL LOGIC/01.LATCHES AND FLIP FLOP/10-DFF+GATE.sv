9cf233b2ecc0a74379ffa737d197847d87289665
Manogaran17 <manogaran2667@gmail.com>
2026-04-18 23:34:58 +0530

    Create 10-DFF+GATE.sv

---
+++ CIRCUITS/02-SEQUENTIAL LOGIC/01.LATCHES AND FLIP FLOP/10-DFF+GATE.sv
@@ -0,0 +1,21 @@
+module top_module (
+    input clk,
+    input in, 
+    output out);
+    wire w;
+    
+    d_ff a(.d(w),.clk(clk),.q(out));
+    assign w = in^out;
+    
+endmodule
+
+module d_ff(d,clk,q);
+    input d,clk;
+    output reg q;
+    
+    always@(posedge clk)
+        begin
+            q<=d;
+        end 
+endmodule 
+   
