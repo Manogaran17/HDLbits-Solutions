2b437d236268abde37fcd57563ba47dd931d718b
Manogaran17 <manogaran2667@gmail.com>
2026-04-18 23:50:23 +0530

    Update 04-SLOW DECADE COUNTER.sv

--- CIRCUITS/02-SEQUENTIAL LOGIC/02-COUNTER/04-SLOW DECADE COUNTER.sv
+++ CIRCUITS/02-SEQUENTIAL LOGIC/02-COUNTER/04-SLOW DECADE COUNTER.sv
@@ -1,1 +1,19 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    
+    reg[3:0]count;
+    always@(posedge clk)
+        begin
+            if(reset ||(count==4'd9&&slowena))
+                count<=0;
+            else if(count<9&&slowena)
+                count<=count+1;
+            else if(slowena==0)
+                count<=count;
+        end 
+    assign q=count;
+endmodule
 
