e55313ae8bd048424dbe8dd6107ec53548468789
Manogaran17 <manogaran2667@gmail.com>
2026-04-18 23:38:32 +0530

    Create 14-CREATE CIRCUIT FROM TRUTH TABLE.sv

---
+++ CIRCUITS/02-SEQUENTIAL LOGIC/01.LATCHES AND FLIP FLOP/14-CREATE CIRCUIT FROM TRUTH TABLE.sv
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output Q); 
+    
+    wire d;
+    assign d = j&~Q|~k&Q;
+    
+    always@(posedge clk)
+        begin
+            Q<=d;
+        end 
+
+endmodule
