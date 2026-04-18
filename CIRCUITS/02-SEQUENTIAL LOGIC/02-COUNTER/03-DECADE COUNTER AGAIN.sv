663efbfa8e7023454f2e07a009eefcb6858b9818
Manogaran17 <manogaran2667@gmail.com>
2026-04-18 23:50:03 +0530

    Update 03-DECADE COUNTER AGAIN.sv

--- CIRCUITS/02-SEQUENTIAL LOGIC/02-COUNTER/03-DECADE COUNTER AGAIN.sv
+++ CIRCUITS/02-SEQUENTIAL LOGIC/02-COUNTER/03-DECADE COUNTER AGAIN.sv
@@ -1,1 +1,20 @@
+module top_module (
+    input clk,
+    input reset,
+    output [3:0] q);
+    
+    reg[3:0]count;
+    
+    always@(posedge clk)
+        begin
+            if(reset)
+                count<=1;
+            else if (count<10)
+                count<=count+1;
+            else 
+                count<=1;
+        end 
+    assign q=count;
+    
+endmodule
 
