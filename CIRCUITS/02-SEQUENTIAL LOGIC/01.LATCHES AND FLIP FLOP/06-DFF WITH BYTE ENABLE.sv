3b75c2f00f8ffd96aa88d9cbaa7d42eae4aeb95d
Manogaran17 <manogaran2667@gmail.com>
2026-04-18 23:31:14 +0530

    Update 06-DFF WITH BYTE ENABLE.sv

--- CIRCUITS/02-SEQUENTIAL LOGIC/01.LATCHES AND FLIP FLOP/06-DFF WITH BYTE ENABLE.sv
+++ CIRCUITS/02-SEQUENTIAL LOGIC/01.LATCHES AND FLIP FLOP/06-DFF WITH BYTE ENABLE.sv
@@ -1,1 +1,37 @@
+module top_module (
+    input clk,
+    input resetn,
+    input [1:0] byteena,
+    input [15:0] d,
+    output [15:0] q
+    
+);
+    integer i,j,k;
+    always@(posedge clk)
+        begin
+            if(!resetn)
+                q<=0;
+            else 
+                begin
+                    if(byteena==2'b01)
+                    begin
+                    for(i=0;i<8;i=i+1)
+                        q[i]<=d[i];
+                    end 
+                    else if (byteena==2'b10)
+                               begin
+                    for(j=8;j<16;j=j+1)
+                        q[j]<=d[j];
+                               end 
+                    else if(byteena==2'b11)
+                    begin
+                        for(k=0;k<16;k=k+1)
+                            q[k]<=d[k];
+                    end 
+                    else if(byteena==2'b00)
+                        q<=q;
+                                     
+                end 
+        end 
+endmodule
 
