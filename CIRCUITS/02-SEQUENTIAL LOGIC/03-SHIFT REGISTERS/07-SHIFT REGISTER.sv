911b09908954880af7db956e5a5e9276fcc44db3
Manogaran17 <manogaran2667@gmail.com>
2026-04-22 23:49:42 +0530

    Update 07-SHIFT REGISTER.sv

--- CIRCUITS/02-SEQUENTIAL LOGIC/03-SHIFT REGISTERS/07-SHIFT REGISTER.sv
+++ CIRCUITS/02-SEQUENTIAL LOGIC/03-SHIFT REGISTERS/07-SHIFT REGISTER.sv
@@ -1,1 +1,28 @@
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
+    wire w1,w2,w3;
+    
+    my_dff a(.d(in),.clk(clk),.rstn(resetn),.q(w1));
+    my_dff b(.d(w1),.clk(clk),.rstn(resetn),.q(w2));
+    my_dff c(.d(w2),.clk(clk),.rstn(resetn),.q(w3));
+    my_dff d(.d(w3),.clk(clk),.rstn(resetn),.q(out));
+    
+endmodule
+
+module my_dff(d,clk,rstn,q);
+    input d,clk,rstn;
+    output reg q;
+    
+    always@(posedge clk)
+        begin
+            if(!rstn)
+                q<=0;
+            else 
+                q<=d;
+        end 
+endmodule 
+    
 
