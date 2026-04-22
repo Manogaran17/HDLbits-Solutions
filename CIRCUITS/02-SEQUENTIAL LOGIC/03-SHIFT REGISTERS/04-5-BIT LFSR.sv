5d0ffc2ae4613c8f329f794579a1ffe4b8a0136e
Manogaran17 <manogaran2667@gmail.com>
2026-04-22 23:47:59 +0530

    Update 04-5-BIT LFSR.sv

--- CIRCUITS/02-SEQUENTIAL LOGIC/03-SHIFT REGISTERS/04-5-BIT LFSR.sv
+++ CIRCUITS/02-SEQUENTIAL LOGIC/03-SHIFT REGISTERS/04-5-BIT LFSR.sv
@@ -1,1 +1,32 @@
+module top_module(
+    input clk,
+    input reset,    // Active-high synchronous reset to 5'h1
+    output [4:0] q
+); 
+    wire w1,w2;
+    
+    my_dff a(.d(w1),.clk(clk),.rst(reset),.rst1(0),.q(q[4]));
+    my_dff b(.d(q[4]),.clk(clk),.rst(reset),.rst1(0),.q(q[3]));
+    my_dff c(.d(w2),.clk(clk),.rst(reset),.rst1(0),.q(q[2]));
+    my_dff d(.d(q[2]),.clk(clk),.rst(reset),.rst1(0),.q(q[1]));
+    my_dff e(.d(q[1]),.clk(clk),.rst(reset),.rst1(1),.q(q[0]));
+    
+    xor g1(w1,1'b0,q[0]);
+    xor g2(w2,q[3],q[0]);
+    
+
+endmodule
+
+module my_dff(d,clk,rst,rst1,q);
+    input d,clk,rst,rst1;
+    output reg q;
+    
+    always@(posedge clk)
+        begin
+            if(rst)
+                q<=rst1;
+            else
+                q<=d;
+        end 
+endmodule 
 
