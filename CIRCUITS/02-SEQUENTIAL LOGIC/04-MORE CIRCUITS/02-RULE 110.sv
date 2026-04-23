1142dd98250a16fbe17d80a55cfee86d5483ecf1
Manogaran17 <manogaran2667@gmail.com>
2026-04-23 23:45:13 +0530

    Update 02-RULE 110.sv

--- CIRCUITS/02-SEQUENTIAL LOGIC/04-MORE CIRCUITS/02-RULE 110.sv
+++ CIRCUITS/02-SEQUENTIAL LOGIC/04-MORE CIRCUITS/02-RULE 110.sv
@@ -1,1 +1,33 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q
+); 
+    integer i;
+    always@(posedge clk)
+        begin
+            if(load)
+                q<=data;
+            else 
+                begin
+                    q[0]<=q[0];
+                    q[511]<=q[511]|q[510];
+                    
+                    for(i=1;i<511;i=i+1)begin
+                        case({q[i+1],q[i],q[i-1]})
+                            3'b000:q[i]<=0;
+                            3'b001:q[i]<=1;
+                            3'b010:q[i]<=1;
+                            3'b011:q[i]<=1;
+                            3'b100:q[i]<=0;
+                            3'b101:q[i]<=1;
+                            3'b110:q[i]<=1;
+                            3'b111:q[i]<=0;
+                        endcase
+                end 
+        end 
+        end 
+
+endmodule
 
