8a20e263063003c75d3261c277ca74d57e2dac25
Manogaran17 <manogaran2667@gmail.com>
2026-04-24 23:33:58 +0530

    Update and rename 02-SIMPLE FSM-2.sv to 02-SIMPLE FSM-1.sv

---
+++ CIRCUITS/02-SEQUENTIAL LOGIC/05-FINITE STATE MACHINES/02-SIMPLE FSM-1.sv
@@ -0,0 +1,32 @@
+// Note the Verilog-1995 module declaration syntax here:
+module top_module(clk, reset, in, out);
+    input clk;
+    input reset;    // Synchronous reset to state B
+    input in;
+    output out;//  
+    reg out;
+ initial out = 0;
+
+    
+	parameter A=0,B=1;
+    reg present_state, next_state;
+
+    always @(*) begin 
+        next_state=present_state~^in;
+    end
+
+    always @(posedge clk) begin    // This is a sequential always block
+        // State flip-flops with asynchronous reset
+        if(reset)
+            present_state<=B;
+        else 
+            present_state<=next_state;
+    end
+
+    // Output logic
+    assign out=present_state;
+
+endmodule
+
+
+
