8a94d7bdac023a988118e6be3747d2ea3076132f
Manogaran17 <manogaran2667@gmail.com>
2026-04-26 23:20:05 +0530

    Update 16-PS\2 PACKET PARSER AND DATA PATH.sv

--- CIRCUITS/02-SEQUENTIAL LOGIC/05-FINITE STATE MACHINES/16-PS\2 PACKET PARSER AND DATA PATH.sv
+++ CIRCUITS/02-SEQUENTIAL LOGIC/05-FINITE STATE MACHINES/16-PS\2 PACKET PARSER AND DATA PATH.sv
@@ -1,1 +1,33 @@
+module top_module(
+    input clk,
+    input [7:0] in,
+    input reset,    // Synchronous reset
+    output [23:0] out_bytes,
+    output done); //
+
+     parameter A=0,B=1,C=2,D=3;
+    reg [1:0] ps,ns;
+
+    always@(*)begin
+        case(ps)
+            A:ns=in[3]?B:A;
+            B:ns=C;
+            C:ns=D;
+            D:ns=in[3]?B:A;
+        endcase
+    end
+    always@(posedge clk)begin
+        if(reset)begin
+            ps<=A;
+            out_bytes<=24'hxxx;
+        end
+    	else begin
+            ps<=ns;
+            out_bytes<={out_bytes[15:0],in};
+        end
+    end
+    
+    assign done=&ps;
+
+endmodule
 
