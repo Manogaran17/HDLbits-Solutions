a84f12b19581dae15bfc6397041f8f064b17ae8e
Manogaran17 <manogaran2667@gmail.com>
2026-04-27 23:52:21 +0530

    Update 09-DESIGN A MOORE FSM.sv

--- CIRCUITS/02-SEQUENTIAL LOGIC/05-FINITE STATE MACHINES/09-DESIGN A MOORE FSM.sv
+++ CIRCUITS/02-SEQUENTIAL LOGIC/05-FINITE STATE MACHINES/09-DESIGN A MOORE FSM.sv
@@ -7,18 +7,20 @@ module top_module (
     output fr1,
     output dfr
 ); 
-    localparam [2:0] A  = 3'd0,	//water level:below s1    
-					 B0 = 3'd1,	//s1~s2, and previous level is higher
-					 B1 = 3'd2,	//s1~s2, and previous level is lower
-					 C0 = 3'd3,	//s2~s3, and previous level is higher
-					 C1 = 3'd4,	//s2~s3, and previous level is lower
-					 D  = 3'd5;	//above s3
+    localparam [2:0] A=3'd0,	//water level:below s1    
+					 B0=3'd1,	//s1~s2, and previous level is higher
+					 B1=3'd2,	//s1~s2, and previous level is lower
+					 C0=3'd3,	//s2~s3, and previous level is higher
+					 C1=3'd4,	//s2~s3, and previous level is lower
+					 D=3'd5;	//above s3
 
 	reg [2:0] state, next_state;
 
-	always @(posedge clk) begin
-		if(reset) state <= A;
-		else state <= next_state;
+	always @(posedge clk)
+		begin
+		if(reset) 
+			state<=A;
+		else state<=next_state;
 	end
 
 	always @(*) begin
